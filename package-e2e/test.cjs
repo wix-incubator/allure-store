3df0fcf0fbfc4fa532b5b68ff793c482685a36ea
Yaroslav Serhieiev <noomorph@gmail.com>
2024-12-07 18:31:56 +0200

    test: fix assertions

--- package-e2e/test.cjs
+++ package-e2e/test.cjs
@@ -3,8 +3,6 @@ const {
   AllureStore,
   fromConfig,
   fromDirectory,
-  AllureReader,
-  AllureWriter,
   FileAllureReader,
   FileAllureWriter,
   // Type exports won't be runtime objects, so we can't assert them directly,
@@ -19,8 +17,3 @@ assert(typeof fromDirectory === 'function', 'fromDirectory should be a function'
 assert(typeof AllureStore === 'function', 'AllureStore should be a class/function');
 assert(typeof FileAllureReader === 'function', 'FileAllureReader should be a class/function');
 assert(typeof FileAllureWriter === 'function', 'FileAllureWriter should be a class/function');
-
-// AllureReader and AllureWriter are interfaces, so they won't have runtime checks.
-// But let's at least assert they aren't undefined.
-assert(AllureReader !== undefined, 'AllureReader should be exported');
-assert(AllureWriter !== undefined, 'AllureWriter should be exported');
--- package-e2e/test.mjs
+++ package-e2e/test.mjs
@@ -3,8 +3,6 @@ import {
   AllureStore,
   fromConfig,
   fromDirectory,
-  AllureReader,
-  AllureWriter,
   FileAllureReader,
   FileAllureWriter,
   // Since MJS also doesn't give us runtime interfaces,
@@ -17,7 +15,3 @@ assert(typeof fromDirectory === 'function', 'fromDirectory should be a function'
 assert(typeof AllureStore === 'function', 'AllureStore should be a class');
 assert(typeof FileAllureReader === 'function', 'FileAllureReader should be a class');
 assert(typeof FileAllureWriter === 'function', 'FileAllureWriter should be a class');
-
-// Interfaces/types won't be runtime constructs, just check they're not undefined.
-assert(AllureReader !== undefined, 'AllureReader should be exported');
-assert(AllureWriter !== undefined, 'AllureWriter should be exported');
